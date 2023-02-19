58ddfba87e0122e498fc3775470d9c8c9337d920
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2023-02-19 11:56:11 -0500

    add upload to s3 and store entry on mongodb

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,16 @@
 import express from "express";
 import multer from "multer";
+import AWS from "aws-sdk";
 
 import Post from "../models/Post.js";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+});
 
 const router = express.Router();
 
@@ -33,56 +39,70 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", upload.single("image"), async (req, res) => {
-  console.log(req.file);
+  try {
+    const uploadedImage = await s3
+      .upload({
+        Body: req.file.buffer,
+        Bucket: process.env.AWS_S3_BUCKETNAME,
+        Key: `${Date.now()}.jpg`,
+      })
+      .promise();
 
-  await Post.create({
-    username: req.body.username,
-    img_url: "https://pixlr.com/images/index/collage.webp",
-  });
+    await Post.create({
+      username: req.body.username,
+      img_url: uploadedImage.Location,
+    });
 
-  res.status(201).json({ result: "OK" });
+    res.status(201).json({ result: "OK" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 router.get("/stats", async (req, res) => {
-  const currentYear = new Date().getFullYear();
+  try {
+    const currentYear = new Date().getFullYear();
 
-  const result = await Post.aggregate([
-    {
-      $project: {
-        year: {
-          $year: "$created_date",
-        },
-        hour: {
-          $hour: "$created_date",
-        },
-      },
-    },
-    {
-      $match: {
-        year: currentYear,
-      },
-    },
-    {
-      $project: {
-        hour: 1,
-      },
-    },
-    {
-      $group: {
-        _id: "$hour",
-        count: {
-          $sum: 1,
+    const result = await Post.aggregate([
+      {
+        $project: {
+          year: {
+            $year: "$created_date",
+          },
+          hour: {
+            $hour: "$created_date",
+          },
         },
       },
-    },
-    {
-      $sort: {
-        _id: 1,
+      {
+        $match: {
+          year: currentYear,
+        },
       },
-    },
-  ]);
+      {
+        $project: {
+          hour: 1,
+        },
+      },
+      {
+        $group: {
+          _id: "$hour",
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+    ]);
 
-  res.json({ result });
+    res.json({ result });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 export default router;
